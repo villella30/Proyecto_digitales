143c78092783d4d2add670072f8e1ca5a49a999e
Villella30 <villellavalentino11@gmail.com>
2022-08-18 13:47:50 -0300

    ordene el codigo

--- proyecto.ino
+++ proyecto.ino
@@ -3,7 +3,7 @@ class Motor
   int pin_dir;
   int pin_step;
   int stepDelay = 1000;
-  
+
 public:
   Motor(int pin_dir, int pin_step)
   {
@@ -14,7 +14,6 @@ public:
     pinMode(pin_step, OUTPUT);
   }
 
-  
   void move()
   {
     digitalWrite(this->pin_step, HIGH);
@@ -22,10 +21,12 @@ public:
     digitalWrite(this->pin_step, LOW);
     delayMicroseconds(stepDelay);
   }
-  void dirUp(){
+  void dirUp()
+  {
     digitalWrite(this->pin_dir, HIGH);
   }
-  void dirDown(){
+  void dirDown()
+  {
     digitalWrite(this->pin_dir, LOW);
   }
   void stop()
@@ -38,15 +39,16 @@ const int stepPin = 8;
 const int steps = 200;
 int stepDelay;
 int estado;
-int dato;
+char dato;
 
-enum MOTORESTADOS{
-    MOTOR_SUBIR,
-    MOTOR_BAJAR,
-    MOTOR_FRENAR
+enum MOTORESTADOS
+{
+  MOTOR_SUBIR,
+  MOTOR_BAJAR,
+  MOTOR_FRENAR
 }
 
-int estado=MOTOR_FRENAR;
+int estado = MOTOR_FRENAR;
 
 #define SUBIR 'F'
 #define BAJAR 'B'
@@ -54,37 +56,46 @@ int estado=MOTOR_FRENAR;
 #define PIN_MOTOR_DIR 2
 #define PIN_MOTOR_STEP 3
 
-Motor *motor= new Motor(PIN_MOTOR_DIR, PIN_MOTOR_STEP);
-void setup() {
-  
+Motor *motor = new Motor(PIN_MOTOR_DIR, PIN_MOTOR_STEP);
+void setup()
+{
+
   Serial.begin(9600);
-  
 }
 
-void loop(){
-if(Serial.available()){
+void loop()
+{
+  if (Serial.available())
+  {
     dato = Serial.read();
-    if(dato==SUBIR)estado= MOTOR_SUBIR;
-    if(dato==BAJAR)estado= MOTOR_BAJAR;
-    if(dato==FRENAR)estado= MOTOR_FRENAR;
-}
+    Serial.println(dato);
+    if (dato == SUBIR)
+      estado = MOTOR_SUBIR;
+    if (dato == BAJAR)
+      estado = MOTOR_BAJAR;
+    if (dato == FRENAR)
+      estado = MOTOR_FRENAR;
+  }
 
-    switch (estado)
-    {
-    case  MOTOR_SUBIR:{
-        motor->dirUp();
-        motor->move();
-        break;
-    }
-     case  MOTOR_BAJAR:{
-        motor->dirDown();
-        motor->move();
-        break;
-    }
-     case  MOTOR_FRENAR: 
-     default: {
-        motor->stop();
-        break;
-    }
-    }
+  switch (estado)
+  {
+  case MOTOR_SUBIR:
+  {
+    motor->dirUp();
+    motor->move();
+    break;
+  }
+  case MOTOR_BAJAR:
+  {
+    motor->dirDown();
+    motor->move();
+    break;
+  }
+  case MOTOR_FRENAR:
+  default:
+  {
+    motor->stop();
+    break;
+  }
+  }
 }
